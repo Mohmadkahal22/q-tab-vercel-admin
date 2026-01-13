210c18b03a5ef9bfd91b2e3b107c0ed6ad4af0cc
Mohmadkahal22 <mohmadalkahal@gmail.com>
2026-01-13 16:26:47 +0300

    .

---
+++ src/utils/constants.js
@@ -0,0 +1,23 @@
+
+
+
+export const BASE_URL = 'http://q-tap.hassan-tech.site/api/';
+export const BASE_URL_IMG = `http://q-tap.hassan-tech.site/`;
+
+
+
+export const WEBSITE_SERVER_URL = "http://q-tap.hassan-tech.site/"
+export const DASHBOARD_SERVER_URL = "http://q-tap.hassan-tech.site/"
+
+
+
+
+
+
+// export const BASE_URL = 'http://127.0.0.1:8000/api/';
+// export const BASE_URL_IMG = `http://127.0.0.1:8000/`;
+
+
+
+// export const WEBSITE_SERVER_URL = "https://www.qutap.co/"
+// export const DASHBOARD_SERVER_URL = "https://dashboard.qutap.co/"
