d3377ced8d3013f3161260ae89eb20acb79d8fa8
Mohmadkahal22 <mohmadalkahal@gmail.com>
2026-01-15 16:29:01 +0300

    >

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,12 +1,12 @@
 
 
 
-export const BASE_URL = 'http://q-tap.hassan-tech.site/api/';
+export const BASE_URL = 'https://q-tap.hassan-tech.site/api/';
 export const BASE_URL_IMG = `https://q-tap.hassan-tech.site/`;
 
 
 
-export const WEBSITE_SERVER_URL = "https://q-tap.hassan-tech.site/"
+export const WEBSITE_SERVER_URL = "https://q-tap-website.vercel.app/"
 export const DASHBOARD_SERVER_URL = "https://q-tab-vercel-admin.vercel.app/"
 
 
